3d2a229ca7fc921caacd307cc0dc9462579ee3e0
Felipeallanf10 <felipeallanf10@outlook.com>
2025-05-08 11:39:11 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div className="w-96 h-[812px] relative bg-gray-200 overflow-hidden">
+          <header className="w-96  bg-sky-950">
+              
+          </header>
+      </div>
+    </>
+  );
+}
